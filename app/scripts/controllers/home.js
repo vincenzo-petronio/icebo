074a362191ab11e0917116dbdde61fea32a66f13
vincenzo.petronio <vincenzo.petronio@gmail.com>
2014-05-27 14:17:44 +0200

    ADD random function

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('iceboApp')
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.rndmData = {};
+    $scope.rndmData.doClick = function(item, event) {
+        var responsePromise = $http.get("http://www.dav91.altervista.org/icebotari/webservice_json.php?action=getFraseRandom");
+
+        responsePromise.success(function(data, status, headers, config) {
+            $scope.rndmData.fromServer = data.frase;
+        });
+        responsePromise.error(function(data, status, headers, config) {
+            alert("AJAX failed!");
+        });
+    };
   });
