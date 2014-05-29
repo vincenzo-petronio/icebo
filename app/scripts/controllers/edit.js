d87e4a6fb528d59fbeb36c150a7ec4f2e6b298d3
vincenzo.petronio <vincenzo.petronio@gmail.com>
2014-05-29 16:12:39 +0200

    many small changes

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -2,11 +2,6 @@
 
 angular.module('iceboApp')
   .controller('EditCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
     $http( {
         method: "POST",
         url: "http://www.dav91.altervista.org/icebotari/webservice_json.php?action=getListaFrasi"
@@ -15,4 +10,18 @@ angular.module('iceboApp')
     }).error(function(data, status, headers, config) {
         alert("AJAX failed!");
     });
+    $scope.addFrase = function(item, event) {
+        console.log("Form Req");
+
+        var responsePromise = $http.get("http://www.dav91.altervista.org/icebotari/webservice_json.php?action=aggiungiFrase&frase=" + $scope.nuovafrase, {});
+        responsePromise.success(function(dataFromServer, status, headers, config) {
+            console.log("Form Res: "+ dataFromServer.errorDescription);
+        });
+        responsePromise.error(function(data, status, headers, config) {
+            alert("Submitting form failed!");
+        });
+    };
+    $scope.editCounter = function(item, event) {
+        console.log("Edit Counter: " + item)
+    };
   });
