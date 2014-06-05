617faf726ff559341e0cdbc30de49752bfcb8b57
vincenzo.petronio <vincenzo.petronio@gmail.com>
2014-06-05 17:08:06 +0200

    ADD counter function & FIX

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -3,25 +3,39 @@
 angular.module('iceboApp')
   .controller('EditCtrl', function ($scope, $http) {
     $http( {
-        method: "POST",
-        url: "http://www.dav91.altervista.org/icebotari/webservice_json.php?action=getListaFrasi"
+        method: 'POST',
+        url: 'http://www.dav91.altervista.org/icebotari/webservice_json.php?action=getListaFrasi'
     }).success(function(data) {
         $scope.listaFrasi = data; // response data
-    }).error(function(data, status, headers, config) {
-        alert("AJAX failed!");
+    }).error(function() {
+        alert('AJAX failed!');
     });
     $scope.addFrase = function(item, event) {
-        console.log("Form Req");
+        console.log('HTTP Req: ' + $scope.nuovafrase);
 
-        var responsePromise = $http.get("http://www.dav91.altervista.org/icebotari/webservice_json.php?action=aggiungiFrase&frase=" + $scope.nuovafrase, {});
-        responsePromise.success(function(dataFromServer, status, headers, config) {
-            console.log("Form Res: "+ dataFromServer.errorDescription);
+        var responsePromise = $http.get('http://www.dav91.altervista.org/icebotari/webservice_json.php?action=aggiungiFrase&frase=' + $scope.nuovafrase, {});
+        responsePromise.success(function(dataFromServer) {
+            console.log('HTTP Res: ' + dataFromServer.errorDescription);
         });
-        responsePromise.error(function(data, status, headers, config) {
-            alert("Submitting form failed!");
+        responsePromise.error(function() {
+            alert('Submitting form failed!');
         });
     };
-    $scope.editCounter = function(item, event) {
-        console.log("Edit Counter: " + item)
+    $scope.editCounter = function(count) {
+        console.log('Edit Counter: ' + this.f.idFrase + " " + count);
+        var url = '';
+        if(count == '1') {
+            url = 'http://www.dav91.altervista.org/icebotari/webservice_json.php?action=incrementaFrase&idFrase=' + this.f.idFrase;
+        } else if(count == '-1') {
+            url = 'http://www.dav91.altervista.org/icebotari/webservice_json.php?action=decrementaFrase&idFrase=' + this.f.idFrase;
+        }
+        console.log('HTTP Req: ' + url);
+        var responsePromise = $http.get(url);
+        responsePromise.success(function(dataFromServer) {
+            console.log('HTTP Res: ' + dataFromServer.errorDescription);
+        });
+        responsePromise.error(function() {
+            alert('HTTP Request failed!');
+        });
     };
   });
--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,21 +2,15 @@
 
 angular.module('iceboApp')
   .controller('HomeCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
     $scope.rndmData = {};
-    $scope.rndmData.doClick = function(item, event) {
-        var responsePromise = $http.get("http://www.dav91.altervista.org/icebotari/webservice_json.php?action=getFraseRandom");
+    $scope.rndmData.doClick = function() {
+        var responsePromise = $http.get('http://www.dav91.altervista.org/icebotari/webservice_json.php?action=getFraseRandom');
 
-        responsePromise.success(function(data, status, headers, config) {
+        responsePromise.success(function(data) {
             $scope.rndmData.fromServer = data.frase;
         });
-        responsePromise.error(function(data, status, headers, config) {
-            alert("AJAX failed!");
+        responsePromise.error(function() {
+            alert('AJAX failed!');
         });
     };
   });
